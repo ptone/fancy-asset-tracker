8be2aaef8d88bc2fbe312684a06bf6d5f37da687
Preston Holmes <ptone@google.com>
2016-03-01 09:15:53 -0800

    auto build increment

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ mkdir temp_build
 
 rm `basename $1`.bin
 rm temp_build/*.*
+awk '{FS=OFS=" " }/define BUILD_VERSION/{$3+=1}1' $1/main.ino > tfile && mv tfile $1/main.ino
 cp $1/*.* temp_build
 
 cp gps-library/*.h temp_build
